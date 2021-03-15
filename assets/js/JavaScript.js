9b71f6cd2706442bd32b2d81a39ff02dd4c65e5e
Chris Onions <c.onions101@gmail.com>
2021-03-16 09:41:49 +1100

    added psudo code

--- assets/js/JavaScript.js
+++ assets/js/JavaScript.js
@@ -1,17 +1,51 @@
-var navButton = document.querySelector(".navButton")
+var navButton = document.querySelector("#navButton");
 
 
-function play() {
-  console.log("var");
-}
+function play_game() {
+  console.log("Playing");
+};
 
-document.getElementById("#navButton").addEventListener("click", function (event) {
-  play();
-  
 
-  document.getElementById("navButton").removeEventListener()
+navButton.addEventListener("click", function (event) {
+  play_game();
 });
 
-play();
 
-// function (event){}
+var question = {
+  question: "what bla bla bla"
+  optionOne: "yes" ,
+  optionTwo: "no",
+  optionFour: "maybe",
+  optionFive: "definatly"
+};
+
+// function (event, key){
+// object to store all Questions and and answers or array?
+// storing index of questions?
+
+// listen for click of the play button 
+
+// when button clicked start game 
+//     start timer function
+//     style page
+//     pull questions and answers from Object
+//     insert questions & answers on page
+//     wait for click  
+
+// function for timer
+//   count for timer
+//   if timer == 0 stop program 
+
+// on click check if answers match 
+//   stop timer
+//   style window
+//   if corect display correct
+//   else incorrect 
+//   THEN time is subtracted from the clock
+//   WHEN all questions are answered or the timer reaches 0
+
+// next question by index?
+
+
+
+
