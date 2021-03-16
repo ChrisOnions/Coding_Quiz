4f6b6bad05d2930e5a681d8342218468726821b9
Chris Onions <c.onions101@gmail.com>
2021-03-16 21:13:51 +1100

    added timer function

--- assets/js/JavaScript.js
+++ assets/js/JavaScript.js
@@ -1,36 +1,80 @@
+var timercount = document.querySelector("#timerDisplay");
+var questionDisplay = document.querySelector("#questionsText");
 var navButton = document.querySelector("#navButton");
+var highScore = document.querySelector(".highscore");
+var answerButtonsElem = document.querySelector("#answerButtons");
 
+var timercountRemaining = 4
+
+var questionArray = [
+  {
+    question: "what bla bla bla",
+    options: ["choice a", " ", " "],
+    answer: "choice a"
+  },
+  {
+    question: "what bla bla bla",
+    options: ["choice a", " ", " "],
+    answer: "choice c"
+  }
+]
 
 function play_game() {
+  countDownTimer();
+  askQuestions();
+
   console.log("Playing");
 };
 
 
+function askQuestions() {
+  answerButtonsElem.innerHTML = ""
+  for (var i = 0; i < questionArray.length; i++) {
+    questionDisplay.textContent = questionArray[i].question
+
+    for (var j = 0; j < questionArray[i].options.length; j++) {
+      var buttonElem = document.createElement("button")
+      buttonElem.setAttribute("class", ".button");
+      buttonElem.value = questionArray[i].options[j]
+      answerButtonsElem.appendChild(buttonElem)
+    }
+  }
+};
+
+
+
+
+
+//timer function
+function countDownTimer() {
+  var timerInterval = setInterval(function () {
+    timercountRemaining--;
+    timercount.textContent = timercountRemaining;
+
+    if (timercountRemaining === 0) {
+      clearInterval(timerInterval);
+      gameOver();
+    }
+  }, 1000);
+}
+function gameOver() {
+  console.log("gameover")
+};
+
+// listen for click of the play button 
 navButton.addEventListener("click", function (event) {
+  console.log(event)
   play_game();
 });
 
-
-var question = {
-  question: "what bla bla bla"
-  optionOne: "yes" ,
-  optionTwo: "no",
-  optionFour: "maybe",
-  optionFive: "definatly"
-};
-
-// function (event, key){
-// object to store all Questions and and answers or array?
-// storing index of questions?
-
-// listen for click of the play button 
-
-// when button clicked start game 
+// when start game button clicked 
 //     start timer function
 //     style page
 //     pull questions and answers from Object
 //     insert questions & answers on page
 //     wait for click  
+//     create buttons on click 
+//        check answer function
 
 // function for timer
 //   count for timer
@@ -44,7 +88,10 @@ var question = {
 //   THEN time is subtracted from the clock
 //   WHEN all questions are answered or the timer reaches 0
 
-// next question by index?
+//   next question by index?
+
+//   endgame function
+
 
 
 
